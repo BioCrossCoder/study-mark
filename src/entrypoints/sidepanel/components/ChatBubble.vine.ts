562bb804a304946fc1b667f34d753373befdfd64
biocrosscoder <viscaria_dignics@163.com>
2026-04-25 08:40:26 +0800

    feat: render llm response as markdown

--- src/entrypoints/sidepanel/components/ChatBubble.vine.ts
+++ src/entrypoints/sidepanel/components/ChatBubble.vine.ts
@@ -1,18 +1,16 @@
 import { Panel } from "primevue";
+import { marked } from "marked";
 
 export default function ChatBubble(props: ChatMessage) {
-  const timeString = props.timestamp.toLocaleString();
   return vine`
     <Panel class="m-2">
       <template #header>
         <i :class="sender"/>
       </template>
-      <p>
-        {{message}}
-      </p>
+      <p v-html="marked.parse(message)"></p>
       <template #footer>
         <div class="flex justify-end">
-          <span>{{timeString}}</span>
+          <span>{{timestamp.toLocaleString()}}</span>
         </div>
       </template>
     </Panel>
