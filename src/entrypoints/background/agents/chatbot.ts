a8a2a9b2272eec899133f884e4a07b10da6ec85e
biocrosscoder <viscaria_dignics@163.com>
2026-05-12 16:00:42 +0800

    refactor: transfer agent usage to high-level api

--- src/entrypoints/background/agents/chatbot.ts
+++ src/entrypoints/background/agents/chatbot.ts
@@ -1,62 +1,70 @@
 import { modelConfig } from "@/stores/config";
-import { createChatAgent } from "../infra/chatAgent";
+import { createGeneralAgent } from "../infra/generalAgent";
 import { chatContext } from "../stores/chat";
 import { ErrorMessage, SignalMessage, TextMessage } from "@/common/types";
-import {
-  AIMessage,
-  AIMessageChunk,
-  HumanMessage,
-} from "@langchain/core/messages";
+import { AIMessageChunk, HumanMessage } from "@langchain/core/messages";
 import { ResultAsync } from "neverthrow";
 import { MessageType, Signal } from "@/common/enums";
 
 export const chatbotAgent = {
-  async answerQuestion(
-    port: globalThis.Browser.runtime.Port,
-    data: TextMessage,
-  ) {
-    const agent = createChatAgent(await modelConfig.getValue());
-    // [CallLLMWithHistoryAsContext]
-    const messages = await chatContext.getValue();
-    const { content } = data;
-    messages.push(new HumanMessage(content));
-    const result = await ResultAsync.fromThrowable((message) =>
-      agent.stream(message),
-    )(messages);
-    if (result.isErr()) {
-      send<ErrorMessage>(port, {
-        type: MessageType.Error,
-        content: (result.error as Error).message,
-      });
-      return;
-    }
-    const stream = result.unwrapOr(null)!; // [/]
-    // [TransmitStreamOutputAndRecordMessage]
-    let message = new AIMessageChunk("");
-    for await (const chunk of stream) {
-      message = message.concat(chunk);
-      const text =
-        typeof chunk.content === "string"
-          ? chunk.content
-          : JSON.stringify(chunk.content);
-      send<TextMessage>(port, {
-        type: MessageType.Text,
-        content: text,
-      });
-    } // [/]
-    // [FinishMessageSendingAndUpdateHistory]
-    send<SignalMessage>(port, {
-      type: MessageType.Signal,
-      content: Signal.Finish,
-    });
-    messages.push(new AIMessage(message));
-    chatContext.setValue(messages); // [/]
-    return;
-  },
-  clearHistory() {
-    chatContext.setValue([]);
-  },
+  answerQuestion,
+  clearHistory,
 };
+
+async function answerQuestion(
+  port: globalThis.Browser.runtime.Port,
+  data: TextMessage,
+) {
+  const agent = createGeneralAgent(await modelConfig.getValue());
+  // [CallLLMWithHistoryAsContext]
+  const messages = await chatContext.getValue();
+  const { content } = data;
+  messages.push(new HumanMessage(content));
+  const result = await ResultAsync.fromThrowable((messages) =>
+    agent.stream({ messages }, { streamMode: "messages" }),
+  )(messages);
+  if (result.isErr()) {
+    send<ErrorMessage>(port, {
+      type: MessageType.Error,
+      content: (result.error as Error).message,
+    });
+    return;
+  }
+  const stream = result.unwrapOr(null)!; // [/]
+  // [TransmitStreamOutputAndRecordMessage]
+  const responseChunks = new Array<AIMessageChunk>();
+  for await (const [chunk] of stream) {
+    if (chunk.type !== "ai") {
+      continue;
+    }
+    responseChunks.push(chunk as AIMessageChunk);
+    const text =
+      typeof chunk.content === "string"
+        ? chunk.content
+        : JSON.stringify(chunk.content);
+    send<TextMessage>(port, {
+      type: MessageType.Text,
+      content: text,
+    });
+  } // [/]
+  // [FinishMessageSendingAndUpdateHistory]
+  send<SignalMessage>(port, {
+    type: MessageType.Signal,
+    content: Signal.Finish,
+  });
+  if (responseChunks.length === 0) {
+    return;
+  }
+  messages.push(
+    responseChunks.reduce((c1, c2) => c1.concat(c2)),
+    new AIMessageChunk(""),
+  );
+  chatContext.setValue(messages); // [/]
+}
+
+function clearHistory() {
+  chatContext.setValue([]);
+}
 
 function send<T>(port: globalThis.Browser.runtime.Port, message: T) {
   port.postMessage(message);
--- src/entrypoints/background/infra/generalAgent.ts
+++ src/entrypoints/background/infra/generalAgent.ts
@@ -3,8 +3,16 @@ import { ModelConfig } from "@/common/types";
 import { ChatAnthropic } from "@langchain/anthropic";
 import { ChatGoogle } from "@langchain/google";
 import { ChatOpenAI } from "@langchain/openai";
+import { createAgent } from "langchain";
 
-export function createChatAgent(config: ModelConfig) {
+export function createGeneralAgent(config: ModelConfig) {
+  const model = createModelAdapter(config);
+  return createAgent({
+    model,
+  });
+}
+
+function createModelAdapter(config: ModelConfig) {
   const { model, baseURL, apiKey } = config;
   switch (config.protocol) {
     case ModelProviderProtocol.OpenAI:
