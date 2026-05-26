1a3993adfc4f23a17fce8e9f908adbad65767399
biocrosscoder <viscaria_dignics@163.com>
2026-05-26 22:16:31 +0800

    refactor: add src directory

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,7 +3,11 @@ import { defineConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ["@wxt-dev/module-react"],
+  srcDir: "src",
   manifest: {
+    host_permissions: ["<all_urls>"],
     default_locale: "en",
+    name: "__MSG_extName__",
+    description: "__MSG_extDescription__",
   },
 });
