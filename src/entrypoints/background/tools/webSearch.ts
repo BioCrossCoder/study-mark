1024001b0cce403910e4a2e0770c97f863c733b0
biocrosscoder <viscaria_dignics@163.com>
2026-06-27 14:39:30 +0800

    feat: optimize async handle

--- src/entrypoints/background/tools/webSearch.ts
+++ src/entrypoints/background/tools/webSearch.ts
@@ -3,15 +3,21 @@ import { tool } from "langchain";
 import { ToolName } from "@/common/enums";
 import z from "zod";
 import { sleep } from "@/common/utils";
+import { Mutex } from "async-mutex";
 
 let inst: Awaited<ReturnType<typeof createWebSearchTool>> | null = null;
-let lastVisit = 0;
+const instMutex = new Mutex();
 export const webSearchTool = {
   async load() {
+    const release = await instMutex.acquire();
     inst ??= await createWebSearchTool();
+    release();
     return inst;
   },
 };
+
+let lastVisit = 0;
+const visitMutex = new Mutex();
 
 async function createWebSearchTool() {
   const bridge = new WebMCPBridge("https://mcp.exa.ai/mcp");
@@ -20,11 +26,14 @@ async function createWebSearchTool() {
   const { name, description, inputSchema } = item;
   return tool(
     async (params) => {
-      while (Date.now() - lastVisit <= 1000) {
-        await sleep(500);
+      const release = await visitMutex.acquire();
+      const gap = Date.now() - lastVisit;
+      if (gap <= 1000) {
+        await sleep(1001 - gap);
       }
       const result = await bridge.callTool(name, params);
       lastVisit = Date.now();
+      release();
       return JSON.stringify(result);
     },
     {
